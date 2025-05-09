a9a3178ae30df0649d683a7c08ba3e144f961035
viktorhex <christopherdillinger200@gmail.com>
2025-05-09 17:24:32 -0400

    update

--- app/home/home.spec.js
+++ app/home/home.spec.js
@@ -1,15 +1,15 @@
 'use strict';
 
-describe('myApp.view1 module', function() {
+describe('myApp.home module', function() {
 
-  beforeEach(module('myApp.view1'));
+  beforeEach(module('myApp.home'));
 
-  describe('view1 controller', function(){
+  describe('home controller', function(){
 
     it('should ....', inject(function($controller) {
       //spec body
-      var view1Ctrl = $controller('View1Ctrl');
-      expect(view1Ctrl).toBeDefined();
+      var homeCtrl = $controller('HomeCtrl');
+      expect(homeCtrl).toBeDefined();
     }));
 
   });
