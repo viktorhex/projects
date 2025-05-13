f219013edfae89575c90fbe2ade558f42d663b5e
viktorhex <christopherdillinger200@gmail.com>
2025-05-13 15:53:32 -0400

    example navigating to details view without custom directive

--- app/components/project-item-list/project-item-list.component.js
+++ app/components/project-item-list/project-item-list.component.js
@@ -5,9 +5,13 @@ angular.
   module('projectItemList').
   component('projectItemList', {
     templateUrl: 'components/project-item-list/project-item-list.template.html',
-    controller: ['Project',
-      function ProjectItemListController(Project) {
+    controller: ['Project', '$location',
+      function ProjectItemListController(Project, $location) {
         this.projects = Project.query();
+
+        this.navigateToProject = function (id) {
+          $location.path('/projects/' + id);
+        }
       }
     ]
   });
